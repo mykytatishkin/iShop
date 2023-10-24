3f911fc5e3d662bcc69f07820a3ed803c3e93d20
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-10-24 16:38:36 +0300

    design upd

---
+++ iShop/Data/Migrations/20231020170034_Auth.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iShop.Data.Migrations
+{
+    public partial class Auth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
